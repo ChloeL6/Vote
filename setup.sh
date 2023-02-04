91ee7749ea981e22743a39d842bda9d48b3ec0af
ChloeL6 <yenchi.le1199@gmail.com>
2023-02-03 18:25:43 -0800

    change python version to 3

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ source venv/bin/activate
 python3.7 -m pip install --upgrade pip setuptools wheel
 
 # install Airflow in the virtual env
-AIRFLOW_VERSION=2.3.2
+AIRFLOW_VERSION=2.5.1
 PYTHON_VERSION=3.7
 CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
 pip install "apache-airflow[async,postgres,google]==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
